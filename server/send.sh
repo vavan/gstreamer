a709a8fd77b711b1d709ce8fa6738a9cb60bfe3e
Vavan <volodymyr.tymoshchuk@gmail.com>
2016-03-18 18:35:31 -0700

    Send script
    -Works on Ubuntu

---
+++ server/send.sh
@@ -0,0 +1,16 @@
+gst-launch-1.0 \
+    autovideosrc \
+    ! video/x-raw, width=640, height=480, framerate=30/1 \
+    ! queue \
+    ! x264enc  bitrate=2000 tune=zerolatency speed-preset=ultrafast \
+    ! queue \
+    ! rtph264pay \
+    ! udpsink host=192.168.0.116 port=5000 sync=false
+
+
+#    ! autovideosink sync=false
+    
+    
+#    ! x264enc \
+#    ! queue \
+
